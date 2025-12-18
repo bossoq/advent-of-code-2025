d9390f5ccaab4d683d95d23d9679bc53805c1d6b
kittipos.w <kittipos.w@kasikornline.com>
2025-12-18 23:23:35 +0700

    Change Day 2 to use math logic

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -28,7 +28,37 @@ pub fn part_one(input: &str) -> Option<u64> {
 
 pub fn part_two(input: &str) -> Option<u64> {
     let mut factors_collection: HashMap<u32, HashSet<u32>> = HashMap::new();
+    // let mut factors_collection: HashMap<u32, HashSet<u32>> = HashMap::from([
+    //     (2, HashSet::from([1])),
+    //     (3, HashSet::from([1])),
+    //     (4, HashSet::from([1, 2])),
+    //     (5, HashSet::from([1])),
+    //     (6, HashSet::from([1, 2, 3])),
+    //     (7, HashSet::from([1])),
+    //     (8, HashSet::from([1, 2, 4])),
+    //     (9, HashSet::from([1, 3])),
+    //     (10, HashSet::from([1, 2, 5])),
+    // ]);
     let mut cache_table: HashMap<[u32; 2], (u64, u64, u64)> = HashMap::new();
+    // let mut cache_table: HashMap<[u32; 2], (u64, u64, u64)> = HashMap::from([
+    //     ([2, 1], (1, 9, 11)),
+    //     ([3, 1], (1, 9, 111)),
+    //     ([4, 1], (1, 9, 1111)),
+    //     ([4, 2], (10, 99, 101)),
+    //     ([5, 1], (1, 9, 11111)),
+    //     ([6, 1], (1, 9, 111111)),
+    //     ([6, 2], (10, 99, 10101)),
+    //     ([6, 3], (100, 999, 1001)),
+    //     ([7, 1], (1, 9, 1111111)),
+    //     ([8, 1], (1, 9, 11111111)),
+    //     ([8, 2], (10, 99, 1010101)),
+    //     ([8, 4], (1000, 9999, 10001)),
+    //     ([9, 1], (1, 9, 111111111)),
+    //     ([9, 3], (100, 999, 1001001)),
+    //     ([10, 1], (1, 9, 1111111111)),
+    //     ([10, 2], (10, 99, 101010101)),
+    //     ([10, 5], (10000, 99999, 100001)),
+    // ]);
     let mut result: u64 = 0;
     input.trim().split(',').for_each(|s| {
         let num = s
@@ -42,6 +72,9 @@ pub fn part_two(input: &str) -> Option<u64> {
         let end = num.iter().last().unwrap();
         for n in *start..=*end {
             let n_len = n.to_string().len() as u32;
+            if n_len == 1 {
+                continue;
+            }
             let factors = match factors_collection.get(&n_len) {
                 Some(facs) => facs,
                 None => {
