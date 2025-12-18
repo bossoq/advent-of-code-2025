e062756618732bd28b3eff1e59f463f0c2791ffb
kittipos.w <kittipos.w@kasikornline.com>
2025-12-18 23:08:55 +0700

    Change Day 2 to use math logic

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -26,6 +27,8 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
+    let mut factors_collection: HashMap<u32, HashSet<u32>> = HashMap::new();
+    let mut cache_table: HashMap<[u32; 2], (u64, u64, u64)> = HashMap::new();
     let mut result: u64 = 0;
     input.trim().split(',').for_each(|s| {
         let num = s
@@ -38,19 +41,46 @@ pub fn part_two(input: &str) -> Option<u64> {
         let start = num.iter().next().unwrap();
         let end = num.iter().last().unwrap();
         for n in *start..=*end {
-            let n_str = n.to_string();
-            let n_len = n_str.len();
-            for i in 1..n_len {
-                if n_len % i != 0 {
-                    continue;
+            let n_len = n.to_string().len() as u32;
+            let factors = match factors_collection.get(&n_len) {
+                Some(facs) => facs,
+                None => {
+                    let mut facs: HashSet<u32> = HashSet::new();
+                    for i in 1..n_len {
+                        if n_len % i == 0 {
+                            facs.insert(i);
+                        }
+                    }
+                    factors_collection.insert(n_len, facs);
+                    factors_collection.get(&n_len).unwrap()
                 }
-                if n_str
-                    .as_bytes()
-                    .chunks(i)
-                    .collect::<Vec<&[u8]>>()
-                    .windows(2)
-                    .all(|w| w[0] == w[1])
-                {
+            };
+            for f in factors {
+                let (start, end, multiplier) = match cache_table.get(&[n_len, *f]) {
+                    Some(vals) => *vals,
+                    None => {
+                        if f == &1 {
+                            let start = 1u64;
+                            let end = 9u64;
+                            let multiplier = "1".repeat(n_len as usize).parse::<u64>().unwrap();
+                            cache_table.insert([n_len, *f], (start, end, multiplier));
+                            (start, end, multiplier)
+                        } else {
+                            let start = 10u64.pow(f - 1);
+                            let end = 10u64.pow(*f) - 1;
+                            let n_group = n_len / f;
+                            let multiplier = start
+                                .to_string()
+                                .repeat(n_group as usize)
+                                .parse::<u64>()
+                                .unwrap()
+                                / start;
+                            cache_table.insert([n_len, *f], (start, end, multiplier));
+                            (start, end, multiplier)
+                        }
+                    }
+                };
+                if n % multiplier == 0 && n / multiplier >= start && n / multiplier <= end {
                     result += n;
                     break;
                 }
@@ -58,6 +88,39 @@ pub fn part_two(input: &str) -> Option<u64> {
         }
     });
     Some(result)
+    // let mut result: u64 = 0;
+    // input.trim().split(',').for_each(|s| {
+    //     let num = s
+    //         .split('-')
+    //         .map(|n| match n.parse::<u64>() {
+    //             Ok(val) => val,
+    //             Err(e) => panic!("Failed to parse number {}: {}", n, e),
+    //         })
+    //         .collect::<Vec<u64>>();
+    //     let start = num.iter().next().unwrap();
+    //     let end = num.iter().last().unwrap();
+    //     for n in *start..=*end {
+    //         let n_str = n.to_string();
+    //         let n_len = n_str.len();
+    //         for i in 1..n_len {
+    //             if n_len % i != 0 {
+    //                 continue;
+    //             }
+    //             if n_str
+    //                 .as_bytes()
+    //                 .chunks(i)
+    //                 .collect::<Vec<&[u8]>>()
+    //                 .windows(2)
+    //                 .all(|w| w[0] == w[1])
+    //             {
+    //                 println!("Found repeating number: {}", n);
+    //                 result += n;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // });
+    // Some(result)
 }
 
 #[cfg(test)]
